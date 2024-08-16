d1bfc6582d5d216d00ecf0bd73e9f1c1475a218d
Kasper Mol <awsumthx@gmail.com>
2024-08-16 21:08:28 +0200

    rsvp

--- main.js
+++ main.js
@@ -1,18 +1,75 @@
-let truthIdx = 0;
-const truths = [
-	'You have chosen the truth',
-	'The truth is out there',
-	'The truth will set you free',
-	'The truth is a lie',
-	'The truth is a lie, but a lie is a truth',
-	'Yes, truth for you',
-	'No, truth for you',
-	'Yes, truth for me',
-	'No, truth for me',
-	'Please, no more truth',
-	'Please, more truth',
-]
+const { createClient } = supabase;
+const SUPABASE_URL = 'https://asvezzujmmovbbaycpqe.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzdmV6enVqbW1vdmJiYXljcHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM4MTc4NzksImV4cCI6MjAzOTM5Mzg3OX0.afW8plWDS1BrHmKATwP4-sxnJfOI4asMEo5lHATn52g';
+const client = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-document.getElementById('truth').addEventListener('click', () => {
-	document.getElementById('truth-destination').innerText = truths[truthIdx++ % truths.length];
+(async () => {
+	const { data, error } = await client.rpc('getVisitorByKey', { value: '' })
+	console.info(data, error);
+})();
+
+const rsvpFind = document.getElementById('rsvp-find');
+const rsvpResult = document.getElementById('rsvp-result');
+const rsvpName = document.getElementById('rsvp-name');
+const rsvpForm = document.getElementById('rsvp-form');
+const rsvpKidsAndBabies = document.getElementById('rsvp-kids-and-babies');
+
+rsvpFind.addEventListener('submit', async event => {
+	event.preventDefault();
+
+	const formData = new FormData(event.target);
+	const key = formData.get('key');
+
+	const { data, error } = await client.rpc('getVisitorByKey', { value: key })
+
+	if (error) {
+		console.error(error);
+		return;
+	}
+
+	if (data === null) {
+		alert('Visitor not found');
+		return;
+	}
+
+	rsvpFind.style.display = 'none';
+	rsvpResult.style.display = 'block';
+	rsvpName.textContent = data.name;
+
+	let adults = data.number_of_adults;
+	let kids = data.number_of_kids;
+	let babies = data.number_of_babies;
+	let savedAt = new Date(data.updated_at);
+
+	document.getElementById('saved-at').textContent = data.updated_at ? 'RSVP last updated ' + savedAt.toLocaleString() : '';
+
+	rsvpForm.addEventListener('change', event => {
+		const formData = new FormData(event.target.form);
+
+		adults = parseInt(formData.get('adults'));
+		kids = adults > 0 ? parseInt(formData.get('kids')) : 0;
+		babies = adults > 0 ? parseInt(formData.get('babies')) : 0;
+
+		rsvpKidsAndBabies.style.display = adults > 0 ? 'block' : 'none';
+	});
+
+	rsvpForm.addEventListener('submit', async event => {
+		event.preventDefault();
+
+		const { data, error } = await client.rpc('updateVisitorRSVP', {
+			adults,
+			kids,
+			babies,
+			value: key,
+		});
+
+		if (error) {
+			console.error(error);
+			return;
+		}
+
+		savedAt = new Date(Date.now());
+
+		document.getElementById('saved-at').textContent = 'RSVP last updated ' + savedAt.toLocaleString();
+	});
 });
